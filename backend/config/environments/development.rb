37053597beb5c383f4d72af2a86ada2437bdcfc7
nisshiy30 <nisshiy30@users.noreply.github.com>
2022-10-27 02:38:47 +0900

    add: bulletをいれる

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -3,6 +3,14 @@
 require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
+  config.after_initialize do
+    Bullet.enable        = true
+    Bullet.alert         = true
+    Bullet.bullet_logger = true
+    Bullet.console       = true
+    Bullet.rails_logger  = true
+  end
+
   config.cache_classes = false
   config.eager_load = false
   config.consider_all_requests_local = true
