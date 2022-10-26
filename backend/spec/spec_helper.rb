4c6d667d139c1c2d1b2a101bebc9afb654be4895
nisshiy30 <nisshiy30@users.noreply.github.com>
2022-10-27 02:35:55 +0900

    add: simplecov

--- backend/spec/spec_helper.rb
+++ backend/spec/spec_helper.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+SimpleCov.start 'rails' do
+  enable_coverage :branch
+  minimum_coverage(line: 80, branch: 60)
+end
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
