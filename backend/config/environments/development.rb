798894bcd4de95f4896119b0f5c29c01e8771d56
nisshiy30 <nisshiy30@users.noreply.github.com>
2022-10-27 03:01:07 +0900

    add: redisをいれる

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -15,20 +15,7 @@ Rails.application.configure do
   config.eager_load = false
   config.consider_all_requests_local = true
   config.server_timing = true
-  if Rails.root.join('tmp/caching-dev.txt').exist?
-    config.action_controller.perform_caching = true
-    config.action_controller.enable_fragment_cache_logging = true
-
-    config.cache_store = :memory_store
-    config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.to_i}"
-    }
-  else
-    config.action_controller.perform_caching = false
-
-    config.cache_store = :null_store
-  end
-
+  config.cache_store = :redis_cache_store, { url: "#{ENV.fetch('REDIS_URL')}/1" }
   config.active_support.deprecation = :log
   config.active_support.disallowed_deprecation = :raise
   config.active_support.disallowed_deprecation_warnings = []
