7163f3becf64fe8da5f26891491cdba935d17e29
nisshiy30 <nisshiy30@users.noreply.github.com>
2022-10-28 20:22:07 +0900

    add: frontendのlint

--- frontend/src/libs/ApiClient.ts
+++ frontend/src/libs/ApiClient.ts
@@ -1,8 +1,8 @@
-import Axios, { AxiosResponse, AxiosError } from 'axios';
+import Axios, { AxiosResponse, AxiosError } from "axios";
 
 export const ApiClient = Axios.create({
   baseURL: `http://localhost:3100`,
-  withCredentials: true
+  withCredentials: true,
 });
 
 ApiClient.interceptors.response.use(
