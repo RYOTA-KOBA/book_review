fb81f83cb97d713263e52a02ccb2c2e40e13778e
RYOTA KOBAYASHI <ryotakobayashi04@gmail.com>
2023-04-26 00:42:13 +0900

    一旦home apiを追加する (#4)
    Co-authored-by: RYOTA-KOBA <r_kobayashi_it@itandi-partner.com>

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api, format: 'json' do
+    resources :home, only: %i[index]
+  end
 end
